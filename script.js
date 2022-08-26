6d5bf705e8785f8927adc419c99cebeaa3c5cbab
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-08-27 00:26:52 +0600

    remove key value

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ const keyValueTemplate = document.querySelector("[data-key-value-template]")
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
+document.querySelector("[data-add-query-param-btn]")
+  .addEventListener("click", () => {
+    queryParamsContainer.append(createKeyValuePair())
+  })
+
+document.querySelector("[data-add-request-header-btn]")
+  .addEventListener("click", () => {
+    requestHeadersContainer.append(createKeyValuePair())
+  })
+
 function createKeyValuePair() {
   const element = keyValueTemplate.content.cloneNode(true)
   element.querySelector("[data-remove-btn]").addEventListener("click", e => {
